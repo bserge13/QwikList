2a53c12c21e7e311f248024d331f75493eabf365
Blake Sergesketter <blakesergesketter@gmail.com>
2024-03-22 11:51:15 -0500

    Addition of dependent: :destroy to parent table, list.

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,8 @@ class List < ApplicationRecord
   validates :title, presence: true 
 
   has_many :items
-
+  dependent: :destroy
+  
   def self.desc_order 
     all.order(created_at: :desc) 
     # most recent list appears first 
