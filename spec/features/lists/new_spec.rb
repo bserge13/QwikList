8c861370d2377e12b946e7db704a1503e3334837
Blake Sergesketter <blakesergesketter@gmail.com>
2024-03-22 16:26:45 -0500

    TEST- mini refactors to aesthetics, all testing passes. Partials added to all pages.

--- spec/features/lists/new_spec.rb
+++ spec/features/lists/new_spec.rb
@@ -9,14 +9,14 @@ RSpec.describe 'Lists' do
       expect(page).to have_content('Create a new list')
 
       expect(page).to have_field :title
-      expect(page).to have_button('Create')
+      expect(page).to have_button('create')
     end
 
     it 'creates a new list and routes back to the dashboard page' do 
       visit new_list_path 
 
       fill_in :title, with: 'Grocery list'
-      click_button('Create')
+      click_button('create')
       expect(current_path).to eq(root_path)
 
       expect(page).to have_button('Grocery list')
@@ -25,7 +25,7 @@ RSpec.describe 'Lists' do
     it 'sad path- flashes an error for an empty field' do 
       visit new_list_path 
 
-      click_button('Create')
+      click_button('create')
       expect(current_path).to eq(new_list_path)
       expect(page).to have_content("A list must have a 'Title' to be created")
     end
