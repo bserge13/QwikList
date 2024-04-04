e566ffa13215438811176df36f47c96621537a86
Blake Sergesketter <blakesergesketter@gmail.com>
2024-04-04 12:19:58 -0500

    CHANGE- going from web scraping for holidays to making an api call. Pivoting direction, full steam ahead, matey .

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,10 +1,16 @@
 class TwilioController < ApplicationController
   def notify_users 
-    holidays = HolidayFacade.all_holidays
-
-    if Date.today == holidays.each { |holiday.date| } 
-    # Needs to be Date.today - (two weeks out?)
-      SmsService.new(body: holiday).call
-    end 
+    # holidays = HolidayFacade.all_holidays
+    
+    # holidays.each do |holiday|
+    # if Date.today == holiday.date 
+    #   SmsService.new(holiday).call
+    # end 
+    
+    
+    # -all_holidays will be a facade method that returns an array of holiday objects 
+    # -holidays needs to be an array of holiday objects a holiday should have a date and title
+    
+    # -HURDLES: making this a feature that's a constant check running to determine the date and the holidays dates. 
   end
 end
--- app/services/sms_service.rb
+++ app/services/sms_service.rb
@@ -6,7 +6,7 @@ class SmsService
   TWILIO_TEST_PHONE = Rails.application.credentials.twilio[:test_phone_num] 
 
   def initialize(holiday)
-    @body_message = "#{holiday.title} is just "
+    @body_message = "#{holiday.title} is this month. May be time for a new leading up to it!"
     @to_phone_num = User.notification_numbers
   end
   
@@ -14,7 +14,6 @@ class SmsService
     client = Twilio::REST::Client.new(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
     message = client.messages.create(
       body: @body_message,
-        # "#{holiday.event} is just around the block! Might need a shopping list?",
       from: TWILIO_FROM_NUMBER,
       to: to(@to_phone_num)
     )
@@ -29,6 +28,3 @@ class SmsService
     to_phone_num
   end
 end
-
-# Twilio::SmsService.new(body: 'hello world!', to_phone_num: User.all_phones).call 
-# The above could go into a new holiday facade to pull in info from an external api for upcoming holidays 
